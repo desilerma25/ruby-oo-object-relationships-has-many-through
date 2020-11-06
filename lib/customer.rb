18c2aa89de92049ae0653441a8c8d0f0b30d0d84
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-05 23:51:39 -0600

    commit tree 211a255f85ac408c3bb4bc2d51a8da580c14f805

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
 class Customer
+
+    attr_accessor :name, :age
+
+    @@all
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip) 
+        # allows us to create new meals as a cust. & 
+        # associate it w/ the customer who created it
+    end
+
+    def meals
+        # cust. will look at all meals, select only ones that 
+        # belong to them
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
   
+    def waiters
+        meals.map do |meal|
+            meal. waiter
+        end
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience 
+    # don't have add. attr bc they don't need to keep track of 
+    # the info. **SINGLE SOURCE OF TRUTH**
+
+    @@all = []
+    
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip) 
+        # allows us to create new meals as a waiter & 
+        # associate it w/ the waiter who created it
+    end
+
+    def meals 
+        # waiter will look at all meals, select only ones that 
+        # belong to them
+        Meal.all.select do |meal|
+            meal.waiter == self 
+        end
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+
+        best_tipped_meal.customer
+    end
+
+
 end
